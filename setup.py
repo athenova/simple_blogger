1c8f0894533e104c7cd087b39c077b4f451cdb01
athenova <aleksey.sergeyev@yandex.com>
2025-03-31 13:27:03 +0300

    Dependency compatibility

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='simple_blogger',
-  version='0.0.35',
+  version='0.0.36',
   author='Aleksey Sergeyev',
   author_email='aleksey.sergeyev@yandex.com',
   description='A simple blogger library',
@@ -10,7 +10,7 @@ setup(
   long_description_content_type='text/markdown',
   url='https://github.com/athenova/simple_blogger',
   packages=find_packages(),
-  install_requires=['pillow>=11.1.0', 
+  install_requires=['pillow>=10.4.0', 
                     'openai>=1.65.1', 
                     'pyTelegramBotAPI>=4.26.0', 
                     'requests>=2.32.3', 
