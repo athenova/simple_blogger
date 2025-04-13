a20664791640a8bac3ee9a1d87bb070b765d4366
athenova <aleksey.sergeyev@yandex.com>
2025-04-13 08:25:35 +0300

    Добавлен блоггер шортсов, некеширующий требует создание временных файлов для конситстентности и особенностях работы moviepy

--- simple_blogger/blogger/shorts/__init__.py
+++ simple_blogger/blogger/shorts/__init__.py
@@ -10,22 +10,27 @@ from simple_blogger.builder.prompt import TaskPromptBuilder, ContentBuilderPromp
 from simple_blogger.builder.content import CachedContentBuilder, ContentBuilder
 from simple_blogger.builder.tools import DurationBuilder
 from simple_blogger.preprocessor.text import MarkdownCleaner, SerialProcessor, EmojiCleaner
-import json
+import tempfile, uuid
 
 class ShortsBlogger(CommonBlogger):
-    def __init__(self, posters, image_count=5, force_rebuild=False, index=None):
+    def __init__(self, posters, image_count=5, index=None):
         self.image_count=image_count
-        super().__init__(posters=posters, force_rebuild=force_rebuild, index=index)
+        super().__init__(posters=posters, index=index)
         
     def _speech_generator(self):
         return YandexSpeechGenerator()
-        
+    
+    def __path_constructor(self):
+        return '.'
+
     def _builder(self):
-        tasks = json.load(open(self._tasks_file_path(), "rt", encoding="UTF-8"))
+        tasks = self._load_tasks()
         task_extractor = TaskExtractor(tasks=tasks, check=self._check_task)
+        root_folder = tempfile.gettempdir()
+        file_suffix = str(uuid.uuid4())
         message_builder = CachedContentBuilder(
                             task_builder=task_extractor,
-                            path_constructor=self._path_constructor,
+                            path_constructor=self.__path_constructor,
                             builder=ContentBuilder(
                                 generator=self._message_generator(), 
                                 prompt_builder=TaskPromptBuilder(
@@ -33,73 +38,72 @@ class ShortsBlogger(CommonBlogger):
                                         prompt_constructor=self._message_prompt_constructor
                                     )
                             ),
-                            force_rebuild=self.force_rebuild,
-                            cache=FileCache(root_folder=self._data_folder(), is_binary=False),
-                            filename="text"
+                            force_rebuild=False,
+                            cache=FileCache(root_folder, is_binary=False),
+                            filename=f"text_{file_suffix}"
                         )
         audio_builder=CachedContentBuilder(
                             task_builder=task_extractor,
-                            path_constructor=self._path_constructor,
+                            path_constructor=self.__path_constructor,
                             builder=ContentBuilder(
                                 generator=self._speech_generator(), 
                                 prompt_builder=ContentBuilderPromptBuilder(content_builder=message_builder),
                                 prompt_processor=SerialProcessor([MarkdownCleaner(), EmojiCleaner()])
                             ),
-                            force_rebuild=self.force_rebuild,
-                            filename="audio",
-                            cache=FileCache(root_folder=self._data_folder())
+                            force_rebuild=False,
+                            filename=f"audio_{file_suffix}",
+                            cache=FileCache(root_folder)
                         )
         duration_builder=DurationBuilder(audio_builder)
         subs_builder=CachedContentBuilder(
                         task_builder=task_extractor,
-                        path_constructor=self._path_constructor,
+                        path_constructor=self.__path_constructor,
                         builder=ContentBuilder(
                             generator=SubsGenerator(duration_constructor=duration_builder.build), 
                             prompt_builder=ContentBuilderPromptBuilder(content_builder=message_builder),
                             prompt_processor=SerialProcessor([MarkdownCleaner(), EmojiCleaner()])
                         ),
-                        force_rebuild=self.force_rebuild,
-                        filename="subs",
-                        cache=FileCache(root_folder=self._data_folder(), is_binary=False)                            
+                        force_rebuild=False,
+                        filename=f"subs_{file_suffix}",
+                        cache=FileCache(root_folder, is_binary=False)                            
                     )        
         image_prompt_builder=ContentBuilderPromptBuilder(
                                 content_builder=CachedContentBuilder(
                                     task_builder=task_extractor,
-                                    path_constructor=self._path_constructor,
+                                    path_constructor=self.__path_constructor,
                                     builder=ContentBuilder(
                                         generator=self._image_prompt_generator(), 
                                         prompt_builder=TaskPromptBuilder(
                                             task_builder=task_extractor,
                                             prompt_constructor=self._image_prompt_prompt_constructor
                                         )),
-                                    force_rebuild=self.force_rebuild,
-                                    filename="image_prompt",
-                                    cache=FileCache(root_folder=self._data_folder(), is_binary=False)
+                                    force_rebuild=False,
+                                    filename=f"image_prompt_{file_suffix}",
+                                    cache=FileCache(root_folder, is_binary=False)
                                 )
                             )
-        video_buidler = CachedContentBuilder(
+        video_builder = CachedContentBuilder(
                             task_builder=task_extractor,
-                            path_constructor=self._path_constructor,
+                            path_constructor=False,
                             builder=ContentBuilder(
-                                generator=ImageSequenceGenerator(self._image_generator(), duration_constructor=duration_builder.build, image_count=self.image_count),
+                                generator=ImageSequenceGenerator(self._image_generator(), duration_builder.build, self.image_count),
                                 prompt_builder=image_prompt_builder,
                             ),
-                            force_rebuild=self.force_rebuild,
-                            cache=FileCache(root_folder=self._data_folder()),
-                            filename="video"
+                            force_rebuild=False,
+                            cache=FileCache(root_folder),
+                            filename=f"video_{file_suffix}"
                         )
-        
-        media_buidler = CachedContentBuilder(
+        media_builder = CachedContentBuilder(
                             task_builder=task_extractor,
-                            path_constructor=self._path_constructor,
-                            builder=ShortsBuilder(audio_builder=audio_builder, subs_builder=subs_builder, video_builder=video_buidler),
-                            force_rebuild=self.force_rebuild,
-                            cache=FileCache(root_folder=self._data_folder()),
-                            filename="media"
+                            path_constructor=self.__path_constructor,
+                            builder=ShortsBuilder(audio_builder, subs_builder, video_builder),
+                            force_rebuild=False,
+                            cache=FileCache(root_folder),
+                            filename=f"media_{file_suffix}"
                         )
 
         builder = PostBuilder(
             message_builder=message_builder,
-            media_builder=media_buidler
+            media_builder=media_builder
         )
         return builder
