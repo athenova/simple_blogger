22051ce943ade19162bdab05a4ebddf21fcfa683
athenova <aleksey.sergeyev@yandex.com>
2025-04-26 16:13:53 +0300

    Новый генератор рисунков

--- simple_blogger/generator/openai.py
+++ simple_blogger/generator/openai.py
@@ -1,7 +1,7 @@
 from simple_blogger.generator import File, TextGenerator, ImageGenerator
 from openai import OpenAI
 from io import StringIO, BytesIO
-import os, requests
+import os, requests, base64
 
 class OpenAiTextGenerator(TextGenerator):
     def __init__(self, system_prompt, api_key_name ='OPENAI_API_KEY', model_name='chatgpt-4o-latest'):
@@ -21,7 +21,32 @@ class OpenAiTextGenerator(TextGenerator):
         return File(self.ext(), StringIO(text))
     
 class OpenAiImageGenerator(ImageGenerator):
-    def __init__(self, api_key_name ='OPENAI_API_KEY', model_name='dall-e-3', system_prompt=None, style_prompt=None, size="1024x1024", quality="standard", output_format="png"):
+    def __init__(self, api_key_name ='OPENAI_API_KEY', model_name='dall-e-3', system_prompt=None, style_prompt=None, size="1024x1024", quality="standard"):
+        self.api_key = os.environ.get(api_key_name)
+        self.model_name=model_name
+        self.system_prompt=system_prompt
+        self.style_prompt=style_prompt
+        self.size=size
+        self.quality=quality
+
+    def generate(self, prompt, **_):
+        client = OpenAI(api_key=self.api_key)
+        prompt = f"{self.system_prompt + '\n' if self.system_prompt else ''}{prompt}{'\n' + self.style_prompt if self.style_prompt else ''}"
+        image_url = client.images.generate(
+            model = self.model_name,
+            prompt = prompt,
+            size = self.size,
+            quality = self.quality,
+            n = 1                
+        ).data[0].url
+        response = requests.get(image_url)
+        return File(self.ext(), BytesIO(response.content))
+    
+    def ext(self):
+        return "png"
+    
+class GptImageGenerator(ImageGenerator):
+    def __init__(self, api_key_name ='OPENAI_API_KEY', model_name='gpt-image-1', system_prompt=None, style_prompt=None, size="1024x1024", quality="medium", output_format="png"):
         self.api_key = os.environ.get(api_key_name)
         self.model_name=model_name
         self.system_prompt=system_prompt
@@ -33,16 +58,14 @@ class OpenAiImageGenerator(ImageGenerator):
     def generate(self, prompt, **_):
         client = OpenAI(api_key=self.api_key)
         prompt = f"{self.system_prompt + '\n' if self.system_prompt else ''}{prompt}{'\n' + self.style_prompt if self.style_prompt else ''}"
-        image_url = client.images.generate(
+        image_base64 = client.images.generate(
             model = self.model_name,
             prompt = prompt,
             size = self.size,
-            quality = self.quality,
-            output_format = self.output_format,
-            n = 1                
-        ).data[0].url
-        response = requests.get(image_url)
-        return File(self.ext(), BytesIO(response.content))
+            quality = self.quality
+        ).data[0].b64_json
+        image_bytes = base64.b64decode(image_base64)
+        return File(self.ext(), BytesIO(image_bytes))
     
     def ext(self):
         return self.output_format
